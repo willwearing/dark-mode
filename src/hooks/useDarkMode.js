85012919ec117d89d638d98bb7834f61108aa550
willwearing <willwearing@gmail.com>
2020-09-30 14:22:03 -0600

    mvp working

--- src/hooks/useDarkMode.js
+++ src/hooks/useDarkMode.js
@@ -2,7 +2,7 @@ import React, { useEffect } from "react";
 import useLocalStorage from "./useLocalStorage";
 
 export const useDarkMode = () => {
-  const [darkMode, setDarkMode] = useLocalStorage();
+  const [darkMode, setDarkMode] = useLocalStorage("dark");
   useEffect(() => {
     var bodyDiv = document.querySelector("body");
     darkMode
